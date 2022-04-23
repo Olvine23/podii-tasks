eb8610e4aa4981e95745b69436595a067c722283
Olvine23 <olivinegeorge@gmal.com>
2022-04-23 13:44:34 -0700

    First commit

---
+++ blogger/lib/blogger.ex
@@ -0,0 +1,28 @@
+defmodule Blogger do
+  @moduledoc """
+  Documentation for `Blogger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blogger.hello()
+      :world
+
+  """
+
+  def hello do
+    IO.puts("Hello")
+
+  end
+  def add do
+    4 + 4
+  end
+
+  def fetch_blog do
+    HTTPoison.get!("https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91")
+  end
+
+end
