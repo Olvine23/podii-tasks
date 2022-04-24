7b495aa2865ec3a80922c9311504546df4e1e5c9
Olvine23 <olivinegeorge@gmal.com>
2022-04-23 17:13:48 -0700

    completed the third task

--- blogger/lib/blogger.ex
+++ blogger/lib/blogger.ex
@@ -30,6 +30,23 @@ defmodule Blogger do
 
   end
 
+  def get(url, headers \\ []) do
+    url
+    |> HTTPoison.get(headers)
+    |> case do
+        {:ok, %{body: raw, status_code: code}} -> {code, raw}
+        {:error, %{reason: reason}} -> {:error, reason}
+       end
+    |> (fn {ok, body} ->
+          body
+          |> Poison.decode(keys: :atoms)
+          |> case do
+               {:ok, parsed} -> {ok, parsed}
+               _ -> {:error, body}
+             end
+        end).()
+  end
+
 
 
 end
